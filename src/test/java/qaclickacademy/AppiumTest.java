b9f8d84744d3869017cf99265b8dc266bddf39e3
Sonali <sonali.sahani93@gmail.com>
2022-01-31 19:25:23 +0530

    second changes

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void testMobileApp()
+	{
+		System.out.println("test mobile app");
+	}
+	
+@Test
+ void testMobileScreen()
+	{
+		System.out.println("test mobile screen");
+	}
+
+}
