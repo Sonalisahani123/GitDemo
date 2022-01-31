dc90f72b59ac8cdda83e3174aee48f1fba7e891c
Sonali <sonali.sahani93@gmail.com>
2022-01-31 20:24:10 +0530

    selenim file change by  gitstuff

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -8,11 +8,13 @@ public class SeleniumTest {
 	public void borwserAutomation()
 	{
 		System.out.println("Browser automation");
+		System.out.println("check for the changes");
 	}
 
 	@Test
 	public void elementUI()
 	{
 		System.out.println("element in UI");
+		System.out.println("check for the 2nd method");
 	}
 }
